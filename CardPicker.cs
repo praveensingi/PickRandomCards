9e7fbb77dbf0e0e501576022d376355c39e723de
singi praveen <singi@gmail.com>
2021-10-22 16:01:03 +0530

    "Completed CardPicker class"

---
+++ CardPicker.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PickRandomCards
+{
+    class CardPicker
+    {
+        static Random random = new Random();
+        public static string[] PickSomeCards(int numOfCards)
+        {
+            string[] pickedCards = new string[numOfCards];
+            for (int i = 0; i < numOfCards; i++)
+            {
+                pickedCards[i] = RandomValue() + " of " + RandomSuit();
+            }
+            return pickedCards;
+        }
+
+        private static string RandomSuit()
+        {
+            int value = random.Next(1, 5);
+            if (value == 1)
+            {
+                return "Spades";
+            }
+            else if (value == 2)
+            {
+                return "Hearts";
+            }
+            else if (value == 3)
+            {
+                return "Clubs";
+            }
+            return "Diamonds";
+        }
+
+        private static string RandomValue()
+        {
+            int value = random.Next(1, 14);
+            if (value == 1)
+            {
+                return "Ace";
+            }
+            else if (value == 11)
+            {
+                return "Jack";
+            }
+            else if (value == 12)
+            {
+                return "Queen";
+            }
+            else if (value == 13)
+            {
+                return "King";
+            }
+            return value.ToString();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,7 +6,19 @@ namespace PickRandomCards
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Enter the number of cards to pick: ");
+            string strNumOfCards = Console.ReadLine();
+            if (int.TryParse(strNumOfCards, out int numOfCards)) {
+                string[] cardsPicked = CardPicker.PickSomeCards(numOfCards);
+                foreach (string card in cardsPicked)
+                {
+                    Console.WriteLine(card);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Please enter a valid number.");
+            }
         }
     }
 }
